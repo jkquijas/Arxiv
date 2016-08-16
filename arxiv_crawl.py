b7481333f47f3ad22ea450c43f0fe047e6a32b8b
jonathan <jkquijas@gmail.com>
2016-08-16 16:40:48 -0600

    first commit

---
+++ arxiv_crawl.py
@@ -0,0 +1,18 @@
+#Crawl through arxiv.org and create a set of files for each chosen search category
+#Programmed by Jonathan Quijas
+#Last modified 07/21/2016
+
+
+import time
+from ArxivDataModule import ArxivCrawler
+
+#Clock in time
+start = time.time()
+
+
+search_field = 'cat'
+n_results = 5
+arxiv = ArxivCrawler(n_results, search_field).crawl()
+#Clock in time
+final_time = time.time() - start
+print 'Finished after ', final_time
